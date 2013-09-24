174385efd3bff9ec5e0a92f8f2422cd3eefcd402
justin.yg.choi <justin.yg.choi@gmail.com>
2013-09-24 13:04:13 +0900

    [진행] Pull Request를 하기 위한 테스트 중. AndroidAnnotation 적용 (BriefingFragment는 제외 for 병준) [알려진 버그 및 이슈] 없음 [기타 코멘트] 아아아~ 혼란스러버~

---
+++ Coco-nut/src/com/egoists/coco_nut/android/ToDoFragment.java
@@ -0,0 +1,10 @@
+package com.egoists.coco_nut.android;
+
+import android.support.v4.app.Fragment;
+
+import com.googlecode.androidannotations.annotations.EFragment;
+
+@EFragment(R.layout.fragment_to_do)
+public class ToDoFragment extends Fragment {
+	
+}
