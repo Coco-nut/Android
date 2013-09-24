bcc5875889ae31b8ca1171bb80f7481962eec800
justin.yg.choi <justin.yg.choi@gmail.com>
2013-09-24 13:04:13 +0900

    [진행] 테스트 겸사겸사 AndroidAnnotation 적용 (BriefingFragment는 제외 for 병준) [알려진 버그 및 이슈] 없음 [기타 코멘트] 병준아~ 이제 받아도 된다. ㅎㅎㅎ

---
+++ Coco-nut/src/com/egoists/coco_nut/android/BriefingFragment.java
@@ -0,0 +1,15 @@
+package com.egoists.coco_nut.android;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BriefingFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_briefing, container, false);
+		return rootView;
+	}
+}
