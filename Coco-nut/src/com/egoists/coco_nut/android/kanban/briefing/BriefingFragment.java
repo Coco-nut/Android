50b9d97dd25c3abfe74c1b56a326c00ccf69772b
BJ <dlqudwns@kaist.ac.kr>
2013-10-19 22:20:27 +0900

    ff

--- Coco-nut/src/com/egoists/coco_nut/android/kanban/briefing/BriefingFragment.java
+++ Coco-nut/src/com/egoists/coco_nut/android/kanban/briefing/BriefingFragment.java
@@ -10,36 +10,37 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.egoists.coco_nut.android.R;
+import com.egoists.coco_nut.android.board.event.AllCardsEvent;
+import com.egoists.coco_nut.android.util.AndLog;
 import com.googlecode.androidannotations.annotations.EFragment;
 
 
 @EFragment(R.layout.fragment_briefing)
 public class BriefingFragment extends Fragment {
 	
-	LinearLayout briefing_layout;
-	LinearLayout gantt_list;
-	LinearLayout wip_list;
-	LinearLayout parti_list;
-	LinearLayout peer_list;
+	GanttView gantview;
+	WIPView wipview;
+	ParticipationView partiview;
+	PeerView peerview;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		setHasOptionsMenu(true);
 		
-		briefing_layout = (LinearLayout) inflater.inflate(R.layout.fragment_briefing, container, false); 
-		gantt_list = (LinearLayout) briefing_layout.findViewById(R.id.pager_kanban_briefing_ganttlist);
-		GanttView gantview = new GanttView(getActivity());
+		LinearLayout briefing_layout = (LinearLayout) inflater.inflate(R.layout.fragment_briefing, container, false); 
+		LinearLayout gantt_list = (LinearLayout) briefing_layout.findViewById(R.id.pager_kanban_briefing_ganttlist);
+		gantview = new GanttView(getActivity());
 		gantt_list.addView(gantview);
 		
-		wip_list = (LinearLayout) briefing_layout.findViewById(R.id.pager_kanban_briefing_wiplist);
-		WIPView wipview = new WIPView(getActivity());
+		LinearLayout wip_list = (LinearLayout) briefing_layout.findViewById(R.id.pager_kanban_briefing_wiplist);
+		wipview = new WIPView(getActivity());
 		wip_list.addView(wipview);
 
-		parti_list = (LinearLayout) briefing_layout.findViewById(R.id.pager_kanban_briefing_partilist);
-		ParticipationView partiview = new ParticipationView(getActivity());
+		LinearLayout parti_list = (LinearLayout) briefing_layout.findViewById(R.id.pager_kanban_briefing_partilist);
+		partiview = new ParticipationView(getActivity());
 		parti_list.addView(partiview);
 		
-		peer_list = (LinearLayout) briefing_layout.findViewById(R.id.pager_kanban_briefing_peerlist);
-		PeerView peerview = new PeerView(getActivity());
+		LinearLayout peer_list = (LinearLayout) briefing_layout.findViewById(R.id.pager_kanban_briefing_peerlist);
+		peerview = new PeerView(getActivity());
 		peer_list.addView(peerview);
 		
 		return briefing_layout;
@@ -49,4 +50,13 @@ public class BriefingFragment extends Fragment {
 	{
 		menu.findItem(R.id.menu_add).setVisible(false);
 	}
+    /**
+     * EventBus로 부터 카드 획득
+     * 리스트에 뿌려준다
+     * @param event
+     */
+    public void onEvent(AllCardsEvent event) {
+        AndLog.d("Get event");
+        ganttview.refresh();
+    }
 }
