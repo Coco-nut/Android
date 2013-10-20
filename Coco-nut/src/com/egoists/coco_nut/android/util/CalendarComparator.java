7c20f402aa92a0f8cd0642a2182956ee4508302b
justin.yg.choi <justin.yg.choi@gmail.com>
2013-10-20 13:13:48 +0900

    누락된 파일들 추가

---
+++ Coco-nut/src/com/egoists/coco_nut/android/util/CalendarComparator.java
@@ -0,0 +1,18 @@
+package com.egoists.coco_nut.android.util;
+
+import java.util.Calendar;
+import java.util.Comparator;
+
+@SuppressWarnings("rawtypes")
+public class CalendarComparator implements Comparator {
+
+    @Override
+    public int compare(Object x, Object y) {
+        Calendar xcal = (Calendar) x;
+        Calendar ycal = (Calendar) y;
+        if ( xcal.before(ycal) ) return -1;
+        if ( xcal.after(ycal) ) return 1;
+        return 0;
+      }
+
+}
